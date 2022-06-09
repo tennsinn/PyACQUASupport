3176192a65289be4abb819da440bbe97b00469ec
两仪天心 <rampage@tennsinn.com>
2022-06-09 20:06:33 +0800

    add back the individual entrance of sequence settings

---
+++ SequenceGlobalConfig.py
@@ -0,0 +1,6 @@
+from MeasurementSupport import *
+
+if __name__ == '__main__':
+    VoiceMeasurementSetting.set_const_var()
+    VoiceMeasurementSetting.set_seq_tag()
+    VoiceMeasurementSetting.set_global_config()
